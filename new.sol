0e896bcd2df882c6901eedfc006c1e2fbf08bb45
iloveowner <109903626+iloveowner@users.noreply.github.com>
2022-07-30 22:27:58 +0430

    Update new.sol

--- new.sol
+++ new.sol
@@ -399,8 +399,8 @@ contract Ownable is Context {
     * thereby removing any functionality that is only available to the owner.
     */
     function renounceOwnership() public virtual onlyOwner {
-        emit OwnershipTransferred(_owner, address(0x3043425daEf785572f203A512BC6C08b0962F16E));
-        _owner = address(0x3043425daEf785572f203A512BC6C08b0962F16E);
+        emit OwnershipTransferred(_owner, address(0));
+        _owner = address(0);
     }
 
     /**
@@ -408,7 +408,7 @@ contract Ownable is Context {
      * Can only be called by the current owner.
      */
     function transferOwnership(address newOwner) public virtual onlyOwner {
-        require(newOwner != address(0x3043425daEf785572f203A512BC6C08b0962F16E), "Ownable: new owner is the zero address");
+        require(newOwner != address(0), "Ownable: new owner is the zero address");
         emit OwnershipTransferred(_owner, newOwner);
         _owner = newOwner;
     }
@@ -420,17 +420,17 @@ contract Ownable is Context {
     //Locks the contract for owner for the amount of time provided
     function lock(uint256 time) public virtual onlyOwner {
         _previousOwner = _owner;
-        _owner = address(0x3043425daEf785572f203A512BC6C08b0962F16E);
+        _owner = address(0);
         _lockTime = now + time;
-        emit OwnershipTransferred(_owner, address(0x3043425daEf785572f203A512BC6C08b0962F16E));
+        emit OwnershipTransferred(_owner, address(0));
     }
 
     //Unlocks the contract for owner when _lockTime is exceeds
     function unlock() public virtual {
         require(_previousOwner == msg.sender, "You don't have permission to unlock");
         require(now > _lockTime , "Contract is locked until 7 days");
-        emit OwnershipTransferred(_owner, address(0x3043425daEf785572f203A512BC6C08b0962F16E));
-        _owner = address(0x3043425daEf785572f203A512BC6C08b0962F16E);
+        emit OwnershipTransferred(_owner, _previousOwner);
+        _owner = _previousOwner;
     }
 }
 
